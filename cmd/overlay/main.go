1312961fedaff6e64148a9a6addee1fd605abb94
Jennifer Johnson <jennifer@storj.io>
2018-12-16 20:56:05 -0500

    updating nodetype todos

--- cmd/overlay/main.go
+++ cmd/overlay/main.go
@@ -101,7 +101,6 @@ func cmdAdd(cmd *cobra.Command, args []string) (err error) {
 		zap.S().Infof("adding node ID: %s; Address: %s", i, a)
 		err = c.Put(process.Ctx(cmd), id, pb.Node{
 			Id: id,
-			// TODO: NodeType is missing
 			Address: &pb.NodeAddress{
 				Transport: 0,
 				Address:   a,
@@ -110,7 +109,7 @@ func cmdAdd(cmd *cobra.Command, args []string) (err error) {
 				FreeBandwidth: 2000000000,
 				FreeDisk:      2000000000,
 			},
-			Type: 1,
+			NodeType: pb.NodeType_STORAGE,
 		})
 		if err != nil {
 			return err
